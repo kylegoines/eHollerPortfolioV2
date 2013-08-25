9104f2f8e60590f031002fe47f1a2970a986d394
kyle goines <thirteenotherslikeme@gmail.com>
2013-08-25 13:03:28 -0400

    wordpressy

--- starkers-master/starkers-master/parts/shared/header.php
+++ starkers-master/starkers-master/parts/shared/header.php
@@ -1,5 +1,11 @@
         <header>
             <a class="eLogo" href="<?php bloginfo( 'url' ); ?>">E</a>
+            <ul class="social">
+                <li><a class="dribbble" href="http://dribbble.com/">dribbble</a></li>
+                <li><a class="linkedin" href="http://www.linkedin.com/">linkedin</a></li>
+                <li><a class="twitter" href="https://twitter.com/">twitter</a></li>
+                <li><a class="facebook" href="https://www.facebook.com/">facebook</a></li>
+            </ul>
             <ul class="nav">
                 <li><a href="">work</a></li>
                 <li><a href="">blog</a></li>
